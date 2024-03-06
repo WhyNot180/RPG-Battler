e75e0a965a071496fda485f0bb18286b06bba0f8
Bence Szucs <bence.szucs200625@gmail.com>
2024-03-06 18:36:27 -0500

    Creates Player Type abstract class

---
+++ RPG-Battler/PlayerType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Battler
+{
+    public abstract class PlayerType
+    {
+        public Stats Stats {  get; set; }
+        public abstract void passive();
+
+        public PlayerType(Stats stats)
+        {
+            Stats = new Stats(stats);
+        }
+
+    }
+}
