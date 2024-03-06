008f4e4b3cb8094bd26bc0e3e01e628ba025c603
Bence Szucs <bence.szucs200625@gmail.com>
2024-03-05 21:56:21 -0500

    Adds stats to player

--- RPG-Battler/Player.cs
+++ RPG-Battler/Player.cs
@@ -9,11 +9,13 @@ namespace RPG_Battler
         private Vector2 position = new Vector2(0,0);
         private static int numberOfPlayers = 0;
         public string Name { get; }
+        public Stats Stats { get; set; }
 
         public Player(string name) 
         {
             numberOfPlayers++;
             this.Name = name;
+            this.Stats = new Stats(100, 100, 30, 20, 25, 10, 15, 30);
         }
 
         public void draw(SpriteBatch _spriteBatch)
--- RPG-Battler/Stats.cs
+++ RPG-Battler/Stats.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RPG_Battler
 {
-    internal class Stats
+    public class Stats
     {
 
         public int HP {  get; set; }
