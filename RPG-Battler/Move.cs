3173f51653b02a3a3c4d0017f7f240aae5426b30
Bence Szucs <bence.szucs200625@gmail.com>
2024-03-08 10:28:32 -0500

    Creates move abstract class

---
+++ RPG-Battler/Move.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Battler
+{
+    public abstract class Move
+    {
+        public string name;
+        protected int cooldown;
+        protected int specialPointsUse;
+        protected int baseAccuracy;
+        protected int chargeTime;
+
+        protected abstract bool calculateHit(int accuracy);
+
+        protected abstract bool calculateDamage(Stats stats);
+
+        public abstract void attack(Stats stats);
+
+    }
+}
