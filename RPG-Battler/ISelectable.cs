bbf3b7fb5a57a5520400405939984e1fc600134d
Bence Szucs <bence.szucs200625@gmail.com>
2024-03-08 21:44:37 -0500

    Adds ISelectable interface

---
+++ RPG-Battler/ISelectable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG_Battler
+{
+    public interface ISelectable<T>
+    {
+        void select(T propertyOfSelected, List<T> otherProperties);
+        string Name { get; }
+        string Description { get; }
+    }
+}
