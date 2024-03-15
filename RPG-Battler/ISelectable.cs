2699f93493e24c750a26af572c3abd29eec1cb35
Bence Szucs <bence.szucs200625@gmail.com>
2024-03-14 22:06:38 -0400

    Removes unused imports

--- RPG-Battler/ISelectable.cs
+++ RPG-Battler/ISelectable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RPG_Battler
+﻿namespace RPG_Battler
 {
     public interface ISelectable
     {
--- RPG-Battler/Player.cs
+++ RPG-Battler/Player.cs
@@ -2,7 +2,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace RPG_Battler
